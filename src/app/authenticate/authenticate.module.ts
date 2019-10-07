efdf82c8ad1413c9990147313cf8f4daeb66f261
Nott Humann <xero0969@gmail.com>
2019-10-07 16:52:34 +0700

    first-upload

---
+++ src/app/authenticate/authenticate.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthenticateRoutingModule } from './authenticate-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthenticateRoutingModule
+  ]
+})
+export class AuthenticateModule { }
---
+++ src/app/editor/editor.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EditorRoutingModule } from './editor-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EditorRoutingModule
+  ]
+})
+export class EditorModule { }
